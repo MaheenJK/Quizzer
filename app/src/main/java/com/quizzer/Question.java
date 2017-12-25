9fbc4398229cea1567e679a6b0dce22848a63a09
MaheenJK <mjamil.bscs15seecs@seecs.edu.pk>
2017-12-25 21:02:29 +0500

    Second Commit

--- app/src/main/java/com/quizzer/Question.java
+++ app/src/main/java/com/quizzer/Question.java
@@ -1,7 +1,9 @@
 package com.quizzer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Question extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Question extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+    }
+
+    public void ques(View v) {
+        startActivity(new Intent(Question.this, Question.class));
+        this.finish();
     }
 }
--- app/src/main/java/com/quizzer/Quiz.java
+++ app/src/main/java/com/quizzer/Quiz.java
@@ -1,7 +1,9 @@
 package com.quizzer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Quiz extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Quiz extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+    }
+
+    public void ques(View v) {
+        startActivity(new Intent(Quiz.this, Question.class));
+        this.finish();
     }
 }
